7954cb7934d3545e0794ed5d92b59fdd8d35b941
Zach Lefler <leaving.valhalla@gmail.com>
2022-01-28 11:56:26 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+// wrapGifts(gifts);
+
+function writeCards(arr, eventName) {
+    const thankYous = []
+    for (let i = 0; i < arr.length; i++) {
+        thankYous.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+}
+    return thankYous
+}
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n)
+        n--
+    }}
+
+countDown(10)
